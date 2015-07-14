5f881dd7681363ccbd54e56b1c47db58ed11a745
ck1981 <craigkooloos@gmail.com>
2015-07-14 13:10:14 +0800

    enable PHP debugging in site/config/environments/staging.php

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,8 +1,15 @@
 <?php
 /* Staging */
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
-define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+//ini_set('display_errors', 0);
+//define('WP_DEBUG_DISPLAY', false);
+//define('SCRIPT_DEBUG', false);
+//define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
 
-define('WP_DEBUG', true); //added to try find reason for site not loading after deploy to remote droplet
+
+
+//added all the below and commented out all the above to try find reason for site not loading after deploy to remote droplet, got the below code from https://css-tricks.com/snippets/wordpress/turn-on-wordpress-error-reporting/, changed last two to true
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
+@ini_set('display_errors', 1);
